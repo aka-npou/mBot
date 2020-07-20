58e49475bca17cbc670ba19a83b451bbb29b963d
aka_npou <npou@mail.ru>
2020-07-20 20:28:06 +0300

    периодическая активность

---
+++ src/main/java/aka_npou/mBot/CheckService.java
@@ -0,0 +1,32 @@
+package aka_npou.mBot;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
+@Getter
+@Setter
+public class CheckService {
+    @Value("${pingtask.url}")
+    private String url;
+
+    @Scheduled(fixedRateString = "${pingtask.period}")
+    public void pingMe() {
+        try {
+            URL url = new URL(getUrl());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/main/java/aka_npou/mBot/MBotApplication.java
+++ src/main/java/aka_npou/mBot/MBotApplication.java
@@ -2,8 +2,10 @@ package aka_npou.mBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MBotApplication {
 
 	public static void main(String[] args) {
